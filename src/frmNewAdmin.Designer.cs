d81d065378973081c82805e5b1b7f757cc3e1fec
Viral3651 <jvillanueva3651@gmail.com>
2025-04-16 15:20:47 -0500

    Implemented new admin form and logic

--- src/frmNewAdmin.Designer.cs
+++ src/frmNewAdmin.Designer.cs
@@ -28,12 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmNewAdmin";
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txtUsername = new TextBox();
+            txtPassword = new TextBox();
+            btnSave = new Button();
+            btnCancel = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(36, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(375, 23);
+            label1.TabIndex = 0;
+            label1.Text = "Please enter username and password:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Verdana", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(36, 82);
+            label2.Name = "label2";
+            label2.Size = new Size(95, 18);
+            label2.TabIndex = 1;
+            label2.Text = "username:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Verdana", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(36, 133);
+            label3.Name = "label3";
+            label3.Size = new Size(92, 18);
+            label3.TabIndex = 2;
+            label3.Text = "password:";
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(153, 82);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(258, 23);
+            txtUsername.TabIndex = 3;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(153, 133);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(258, 23);
+            txtPassword.TabIndex = 4;
+            // 
+            // btnSave
+            // 
+            btnSave.BackColor = SystemColors.Highlight;
+            btnSave.Font = new Font("Verdana", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnSave.ForeColor = SystemColors.ButtonHighlight;
+            btnSave.Location = new Point(88, 194);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(100, 40);
+            btnSave.TabIndex = 5;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = false;
+            btnSave.Click += btnSave_Click;
+            // 
+            // btnCancel
+            // 
+            btnCancel.BackColor = SystemColors.Highlight;
+            btnCancel.Font = new Font("Verdana", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnCancel.ForeColor = SystemColors.ButtonHighlight;
+            btnCancel.Location = new Point(250, 194);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(100, 40);
+            btnCancel.TabIndex = 6;
+            btnCancel.Text = "Cancel";
+            btnCancel.UseVisualStyleBackColor = false;
+            btnCancel.Click += btnCancel_Click;
+            // 
+            // frmNewAdmin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(466, 277);
+            Controls.Add(btnCancel);
+            Controls.Add(btnSave);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUsername);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "frmNewAdmin";
+            Text = "frmNewAdmin";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtUsername;
+        private TextBox txtPassword;
+        private Button btnSave;
+        private Button btnCancel;
     }
 }
--- src/frmNewAdmin.cs
+++ src/frmNewAdmin.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System.Security.Cryptography;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +9,144 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections;
 
 namespace WUMedCoProject
 {
     public partial class frmNewAdmin : Form
     {
+        static SqlConnection conn = null!;
+        static SqlCommand cmd = null!;
+        static SqlDataReader reader = null!;
         public frmNewAdmin()
         {
             InitializeComponent();
+        }
+
+        /***********************************************************************
+         * Method to connect to database
+         **********************************************************************/
+        private void Database_Connection()
+        {
+            // Static connection string to connect to the database
+            string url = WUMedCoPath._connectionString;
+
+            conn = new SqlConnection(url);
+        }
+
+        /***********************************************************************
+         * Method to save new admin on button click
+         **********************************************************************/
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            if(ValidateInput(username, password))
+            {
+                SaveNewAdmin(username, password);
+                this.Close();
+            }
+        }
+
+        /***********************************************************************
+         * Method to cancel out of new admin form on button click
+         **********************************************************************/
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /***********************************************************************
+         * Method to validate text fields and password constraints
+         **********************************************************************/
+        private bool ValidateInput(string username, string password)
+        {
+            string msg = "";
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                if (string.IsNullOrEmpty(username))
+                {
+                    msg += "Username cannot be blank.\n\n";
+                }
+                else
+                {
+                    msg += "Password cannot be blank.\n\n";
+                }
+            }
+            else //I organized it this way so that password constraints are only checked if the user enters something. Otherwise if either is blank, it will show ALL error messages
+            {
+                if (password.Length < 8)
+                {
+                    msg += "Password must be at least 8 characters long.\n\n";
+                }
+                if (!password.Any(char.IsUpper))
+                {
+                    msg += "Password must contain at least one uppercase letter.\n\n";
+                }
+                if (!password.Any(char.IsLower))
+                {
+                    msg += "Password must contain at least one lowercase letter.\n\n";
+                }
+                if (!password.Any(char.IsDigit))
+                {
+                    msg += "Password must contain at least one digit.\n\n";
+                }
+                if (!password.Any(ch => "!@#$%^&*()_+[]{}|;:,.<>?".Contains(ch)))
+                {
+                    msg += "Password must contain at least one special character.\n\n";
+                }
+            }
+
+            if(msg != "")
+            {
+                MessageBox.Show(msg, "Error");
+                return false;
+            }
+            else { return true; }
+
+        }
+
+        /***********************************************************************
+         * Method to save new admin to database
+         **********************************************************************/
+        private void SaveNewAdmin(string username, string password)
+        {
+            // Hash the password
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(password);
+                byte[] inputHashBytes = sha256.ComputeHash(inputBytes);
+                string hashedPassword = Convert.ToBase64String(inputHashBytes);
+
+                // Connect and insert into database
+                using (SqlConnection connection = new SqlConnection(WUMedCoPath._connectionString))
+                {
+                    string query = @"INSERT INTO AdminLogin (Username, Password) VALUES (@Username, @Password)";
+
+                    using (SqlCommand cmd = new SqlCommand(query, connection))
+                    {
+                        cmd.Parameters.AddWithValue("@Username", username);
+                        cmd.Parameters.AddWithValue("@Password", hashedPassword);
+
+                        try
+                        {
+                            connection.Open();
+                            cmd.ExecuteNonQuery();
+                            MessageBox.Show("New admin created successfully.", "Success");
+                        }
+                        catch (SqlException ex)
+                        {
+                            MessageBox.Show($"Database error: {ex.Message}", "Error");
+                        }
+                        finally
+                        {
+                            connection.Close();
+                        }
+                    }
+                }
+            }
         }
     }
 }
