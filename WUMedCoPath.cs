8a7e3be4b447b674636d6f69a63ca753957b0493
Viral3651 <jvillanueva3651@gmail.com>
2025-04-16 00:12:23 -0500

    Added class for static connection string

---
+++ WUMedCoPath.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WUMedCoProject
+{
+    internal class WUMedCoPath
+    {
+        public static string _connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\jvill\Documents\Classes\2025SpringWu\DBMS\SemesterProject\WUMedCoProject\WUMedCo.mdf;Integrated Security=True";
+    }
+}
