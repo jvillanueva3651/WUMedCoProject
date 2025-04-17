68bd60821ab4988caf27d7f6cbcad275276886cb
Viral3651 <jvillanueva3651@gmail.com>
2025-04-17 16:28:03 -0500

    Designed AdminHome form and added button navigation logic

--- src/frmAdminHome.Designer.cs
+++ src/frmAdminHome.Designer.cs
@@ -28,12 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmAdminHome";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAdminHome));
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            btnPatients = new Button();
+            btnAppointments = new Button();
+            btnMedRecords = new Button();
+            btnEmployees = new Button();
+            btnDepartments = new Button();
+            btnBuildings = new Button();
+            btnEquipment = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BorderStyle = BorderStyle.Fixed3D;
+            label1.Font = new Font("Verdana", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.Highlight;
+            label1.Location = new Point(226, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(475, 44);
+            label1.TabIndex = 0;
+            label1.Text = "Welcome to WUMedCo!";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(343, 135);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(222, 222);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // btnPatients
+            // 
+            btnPatients.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnPatients.Location = new Point(98, 135);
+            btnPatients.Name = "btnPatients";
+            btnPatients.Size = new Size(124, 48);
+            btnPatients.TabIndex = 2;
+            btnPatients.Text = "Patients";
+            btnPatients.UseVisualStyleBackColor = true;
+            btnPatients.Click += btnPatients_Click;
+            // 
+            // btnAppointments
+            // 
+            btnAppointments.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnAppointments.Location = new Point(82, 214);
+            btnAppointments.Name = "btnAppointments";
+            btnAppointments.Size = new Size(156, 48);
+            btnAppointments.TabIndex = 3;
+            btnAppointments.Text = "Appointments";
+            btnAppointments.UseVisualStyleBackColor = true;
+            btnAppointments.Click += btnAppointments_Click;
+            // 
+            // btnMedRecords
+            // 
+            btnMedRecords.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnMedRecords.Location = new Point(98, 294);
+            btnMedRecords.Name = "btnMedRecords";
+            btnMedRecords.Size = new Size(124, 63);
+            btnMedRecords.TabIndex = 4;
+            btnMedRecords.Text = "Medical Records";
+            btnMedRecords.UseVisualStyleBackColor = true;
+            btnMedRecords.Click += btnMedRecords_Click;
+            // 
+            // btnEmployees
+            // 
+            btnEmployees.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnEmployees.Location = new Point(609, 135);
+            btnEmployees.Name = "btnEmployees";
+            btnEmployees.Size = new Size(124, 48);
+            btnEmployees.TabIndex = 5;
+            btnEmployees.Text = "Employees";
+            btnEmployees.UseVisualStyleBackColor = true;
+            btnEmployees.Click += btnEmployees_Click;
+            // 
+            // btnDepartments
+            // 
+            btnDepartments.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnDepartments.Location = new Point(670, 214);
+            btnDepartments.Name = "btnDepartments";
+            btnDepartments.Size = new Size(153, 48);
+            btnDepartments.TabIndex = 6;
+            btnDepartments.Text = "Departments";
+            btnDepartments.UseVisualStyleBackColor = true;
+            btnDepartments.Click += btnDepartments_Click;
+            // 
+            // btnBuildings
+            // 
+            btnBuildings.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnBuildings.Location = new Point(750, 135);
+            btnBuildings.Name = "btnBuildings";
+            btnBuildings.Size = new Size(124, 48);
+            btnBuildings.TabIndex = 7;
+            btnBuildings.Text = "Buildings";
+            btnBuildings.UseVisualStyleBackColor = true;
+            btnBuildings.Click += btnBuildings_Click;
+            // 
+            // btnEquipment
+            // 
+            btnEquipment.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnEquipment.Location = new Point(670, 294);
+            btnEquipment.Name = "btnEquipment";
+            btnEquipment.Size = new Size(153, 48);
+            btnEquipment.TabIndex = 8;
+            btnEquipment.Text = "Equipment";
+            btnEquipment.UseVisualStyleBackColor = true;
+            btnEquipment.Click += btnEquipment_Click;
+            // 
+            // frmAdminHome
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(915, 426);
+            Controls.Add(btnEquipment);
+            Controls.Add(btnBuildings);
+            Controls.Add(btnDepartments);
+            Controls.Add(btnEmployees);
+            Controls.Add(btnMedRecords);
+            Controls.Add(btnAppointments);
+            Controls.Add(btnPatients);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Name = "frmAdminHome";
+            Text = "frmAdminHome";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Button btnPatients;
+        private Button btnAppointments;
+        private Button btnMedRecords;
+        private Button btnEmployees;
+        private Button btnDepartments;
+        private Button btnBuildings;
+        private Button btnEquipment;
     }
 }
--- src/frmAdminHome.cs
+++ src/frmAdminHome.cs
@@ -16,5 +16,89 @@ namespace WUMedCoProject.src
         {
             InitializeComponent();
         }
+
+        /***********************************************************************
+         * Method to handle the click event of the Patients button.
+         **********************************************************************/
+        private void btnPatients_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            frmPatientCRUD patientsCRUD = new frmPatientCRUD();
+            patientsCRUD.FormClosed += (s, args) => this.Show();
+            patientsCRUD.Show();
+        }
+
+        /***********************************************************************
+         * Method to handle the click event of the Appointments button.
+         **********************************************************************/
+        private void btnAppointments_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            frmAppointmentsCRUD appointmentsCRUD = new frmAppointmentsCRUD();
+            appointmentsCRUD.FormClosed += (s, args) => this.Show();
+            appointmentsCRUD.Show();
+        }
+
+        /***********************************************************************
+         * Method to handle the click event of the Medical Records button.
+         **********************************************************************/
+        private void btnMedRecords_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            frmMedicalRecordsCRUD medicalRecordsCRUD = new frmMedicalRecordsCRUD();
+            medicalRecordsCRUD.FormClosed += (s, args) => this.Show();
+            medicalRecordsCRUD.Show();
+        }
+
+        /***********************************************************************
+         * Method to handle the click event of the Employees button.
+         **********************************************************************/
+        private void btnEmployees_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            frmEmployeeCRUD employeesCRUD = new frmEmployeeCRUD();
+            employeesCRUD.FormClosed += (s, args) => this.Show();
+            employeesCRUD.Show();
+        }
+
+        /***********************************************************************
+         * Method to handle the click event of the Buildings button.
+         **********************************************************************/
+        private void btnBuildings_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            frmBuildingsCRUD buildingsCRUD = new frmBuildingsCRUD();
+            buildingsCRUD.FormClosed += (s, args) => this.Show();
+            buildingsCRUD.Show();
+        }
+
+        /***********************************************************************
+         * Method to handle the click event of the Departments button.
+         **********************************************************************/
+        private void btnDepartments_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            frmDepartmentsCRUD departmentsCRUD = new frmDepartmentsCRUD();
+            departmentsCRUD.FormClosed += (s, args) => this.Show();
+            departmentsCRUD.Show();
+        }
+
+        /***********************************************************************
+         * Method to handle the click event of the Equipment button.
+         **********************************************************************/
+        private void btnEquipment_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            frmEquipmentCRUD equipmentCRUD = new frmEquipmentCRUD();
+            equipmentCRUD.FormClosed += (s, args) => this.Show();
+            equipmentCRUD.Show();
+        }
     }
 }
