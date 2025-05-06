dfd41f904c22ca9a83519b166e47f314d422e014
Joshua Villanueva <jvillanueva3651@gmail.com>
2025-05-06 00:40:04 -0500

    Designed Departments CRUD form. Added basic implementation and outline

--- src/frmDepartmentsCRUD.Designer.cs
+++ src/frmDepartmentsCRUD.Designer.cs
@@ -28,12 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmDepartmentsCRUD";
+            dgvDepartments = new DataGridView();
+            dgvDepartmentID = new DataGridViewTextBoxColumn();
+            dgvDepartmentName = new DataGridViewTextBoxColumn();
+            dgvHeadOfDepartment = new DataGridViewTextBoxColumn();
+            dgvBtnEdit = new DataGridViewButtonColumn();
+            dgvBtnDelete = new DataGridViewButtonColumn();
+            btnAddNewDepartment = new Button();
+            btnReturnHome = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgvDepartments).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvDepartments
+            // 
+            dgvDepartments.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvDepartments.Columns.AddRange(new DataGridViewColumn[] { dgvDepartmentID, dgvDepartmentName, dgvHeadOfDepartment, dgvBtnEdit, dgvBtnDelete });
+            dgvDepartments.Location = new Point(12, 12);
+            dgvDepartments.Name = "dgvDepartments";
+            dgvDepartments.Size = new Size(776, 393);
+            dgvDepartments.TabIndex = 0;
+            dgvDepartments.CellClick += dgvDepartments_CellClick;
+            // 
+            // dgvDepartmentID
+            // 
+            dgvDepartmentID.DataPropertyName = "dgvDepartmentID";
+            dgvDepartmentID.HeaderText = "DepartmentID";
+            dgvDepartmentID.Name = "dgvDepartmentID";
+            dgvDepartmentID.ReadOnly = true;
+            // 
+            // dgvDepartmentName
+            // 
+            dgvDepartmentName.DataPropertyName = "dgvDepartmentName";
+            dgvDepartmentName.HeaderText = "Department";
+            dgvDepartmentName.Name = "dgvDepartmentName";
+            dgvDepartmentName.ReadOnly = true;
+            dgvDepartmentName.Width = 254;
+            // 
+            // dgvHeadOfDepartment
+            // 
+            dgvHeadOfDepartment.DataPropertyName = "dgvHeadOfDepartment";
+            dgvHeadOfDepartment.HeaderText = "Department Head";
+            dgvHeadOfDepartment.Name = "dgvHeadOfDepartment";
+            dgvHeadOfDepartment.ReadOnly = true;
+            dgvHeadOfDepartment.Width = 250;
+            // 
+            // dgvBtnEdit
+            // 
+            dgvBtnEdit.DataPropertyName = "dgvBtnEdit";
+            dgvBtnEdit.HeaderText = "";
+            dgvBtnEdit.Name = "dgvBtnEdit";
+            dgvBtnEdit.UseColumnTextForButtonValue = true;
+            dgvBtnEdit.Width = 65;
+            // 
+            // dgvBtnDelete
+            // 
+            dgvBtnDelete.DataPropertyName = "dgvButtonDelete";
+            dgvBtnDelete.HeaderText = "";
+            dgvBtnDelete.Name = "dgvBtnDelete";
+            dgvBtnDelete.UseColumnTextForButtonValue = true;
+            dgvBtnDelete.Width = 65;
+            // 
+            // btnAddNewDepartment
+            // 
+            btnAddNewDepartment.Location = new Point(628, 411);
+            btnAddNewDepartment.Name = "btnAddNewDepartment";
+            btnAddNewDepartment.Size = new Size(160, 27);
+            btnAddNewDepartment.TabIndex = 1;
+            btnAddNewDepartment.Text = "Add New Department";
+            btnAddNewDepartment.UseVisualStyleBackColor = true;
+            btnAddNewDepartment.Click += btnAddNewDepartment_Click;
+            // 
+            // btnReturnHome
+            // 
+            btnReturnHome.Location = new Point(12, 411);
+            btnReturnHome.Name = "btnReturnHome";
+            btnReturnHome.Size = new Size(160, 27);
+            btnReturnHome.TabIndex = 2;
+            btnReturnHome.Text = "Return to Home";
+            btnReturnHome.UseVisualStyleBackColor = true;
+            btnReturnHome.Click += btnReturnHome_Click;
+            // 
+            // frmDepartmentsCRUD
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnReturnHome);
+            Controls.Add(btnAddNewDepartment);
+            Controls.Add(dgvDepartments);
+            Name = "frmDepartmentsCRUD";
+            Text = "frmDepartmentsCRUD";
+            ((System.ComponentModel.ISupportInitialize)dgvDepartments).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private DataGridView dgvDepartments;
+        private Button btnAddNewDepartment;
+        private Button btnReturnHome;
+        private DataGridViewTextBoxColumn dgvDepartmentID;
+        private DataGridViewTextBoxColumn dgvDepartmentName;
+        private DataGridViewTextBoxColumn dgvHeadOfDepartment;
+        private DataGridViewButtonColumn dgvBtnEdit;
+        private DataGridViewButtonColumn dgvBtnDelete;
     }
 }
--- src/frmDepartmentsCRUD.cs
+++ src/frmDepartmentsCRUD.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WUMedCoProject.src;
 
 namespace WUMedCoProject
 {
@@ -15,6 +16,62 @@ namespace WUMedCoProject
         public frmDepartmentsCRUD()
         {
             InitializeComponent();
+        }
+
+        /**********************************************************************
+         * Method to handle return home button click 
+         *********************************************************************/
+        private void btnReturnHome_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /**********************************************************************
+         * Method to handle add new department button click 
+         *********************************************************************/
+        private void btnAddNewDepartment_Click(object sender, EventArgs e)
+        {
+            OpenDepartmentForm();
+        }
+
+        /**********************************************************************
+         * Method to handle the edit and delete DGV button clicks 
+         *********************************************************************/
+        private void dgvDepartments_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0 || e.ColumnIndex < 0) return;
+
+            var dataTable = (DataTable)dgvDepartments.DataSource;
+
+            var departmentId = Convert.ToInt32(dataTable.Rows[e.RowIndex]["DepartmentID"]);
+
+            //For Debugging
+            //MessageBox.Show($"DepartmentID: {departmentId}");
+
+            if (dgvDepartments.Columns[e.ColumnIndex] == dgvBtnEdit)
+            {
+                OpenDepartmentForm();
+            }
+            else if (dgvDepartments.Columns[e.ColumnIndex] == dgvBtnDelete)
+            {
+                DeleteDepartment(departmentId);
+            }
+        }
+
+        /**********************************************************************
+         * Method to delete a department from the database
+         *********************************************************************/
+        private void DeleteDepartment(int departmentId)
+        {
+            //TODO: Add deletion logic
+        }
+
+        /**********************************************************************
+         * Method to open the department form
+         *********************************************************************/
+        private void OpenDepartmentForm()
+        {
+            
         }
     }
 }
