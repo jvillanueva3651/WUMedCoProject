d1b20d3ef37ae853a45f26ba1eeae29092f1fae4
jvillanueva3651 <jvillanueva3651@gmail.com>
2025-05-06 10:44:27 -0500

    Started designing Employee CRUD from. WIP

--- src/frmEmployeeCRUD.Designer.cs
+++ src/frmEmployeeCRUD.Designer.cs
@@ -28,12 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmEmployeeCRUD";
+            dataGridView1 = new DataGridView();
+            dgvEmployeeID = new DataGridViewTextBoxColumn();
+            dgvLastName = new DataGridViewTextBoxColumn();
+            dgvFirstName = new DataGridViewTextBoxColumn();
+            dgvDoB = new DataGridViewTextBoxColumn();
+            dgvPosition = new DataGridViewTextBoxColumn();
+            dgvExtension = new DataGridViewTextBoxColumn();
+            dgvView = new DataGridViewButtonColumn();
+            dgvEdit = new DataGridViewButtonColumn();
+            dgvDelete = new DataGridViewButtonColumn();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { dgvEmployeeID, dgvLastName, dgvFirstName, dgvDoB, dgvPosition, dgvExtension, dgvView, dgvEdit, dgvDelete });
+            dataGridView1.Location = new Point(12, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(1000, 533);
+            dataGridView1.TabIndex = 0;
+            // 
+            // dgvEmployeeID
+            // 
+            dgvEmployeeID.HeaderText = "EmployeeID";
+            dgvEmployeeID.Name = "dgvEmployeeID";
+            dgvEmployeeID.ReadOnly = true;
+            // 
+            // dgvLastName
+            // 
+            dgvLastName.HeaderText = "Last Name";
+            dgvLastName.Name = "dgvLastName";
+            dgvLastName.ReadOnly = true;
+            // 
+            // dgvFirstName
+            // 
+            dgvFirstName.HeaderText = "First Name";
+            dgvFirstName.Name = "dgvFirstName";
+            dgvFirstName.ReadOnly = true;
+            // 
+            // dgvDoB
+            // 
+            dgvDoB.HeaderText = "Date of Birth";
+            dgvDoB.Name = "dgvDoB";
+            dgvDoB.ReadOnly = true;
+            // 
+            // dgvPosition
+            // 
+            dgvPosition.HeaderText = "Position";
+            dgvPosition.Name = "dgvPosition";
+            dgvPosition.ReadOnly = true;
+            // 
+            // dgvExtension
+            // 
+            dgvExtension.HeaderText = "Extension";
+            dgvExtension.Name = "dgvExtension";
+            dgvExtension.ReadOnly = true;
+            // 
+            // dgvView
+            // 
+            dgvView.HeaderText = "";
+            dgvView.Name = "dgvView";
+            // 
+            // dgvEdit
+            // 
+            dgvEdit.HeaderText = "";
+            dgvEdit.Name = "dgvEdit";
+            // 
+            // dgvDelete
+            // 
+            dgvDelete.HeaderText = "";
+            dgvDelete.Name = "dgvDelete";
+            // 
+            // frmEmployeeCRUD
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1239, 641);
+            Controls.Add(dataGridView1);
+            Name = "frmEmployeeCRUD";
+            Text = "frmEmployeeCRUD";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn dgvEmployeeID;
+        private DataGridViewTextBoxColumn dgvLastName;
+        private DataGridViewTextBoxColumn dgvFirstName;
+        private DataGridViewTextBoxColumn dgvDoB;
+        private DataGridViewTextBoxColumn dgvPosition;
+        private DataGridViewTextBoxColumn dgvExtension;
+        private DataGridViewButtonColumn dgvView;
+        private DataGridViewButtonColumn dgvEdit;
+        private DataGridViewButtonColumn dgvDelete;
     }
 }
