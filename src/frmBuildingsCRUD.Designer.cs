480bc99039d3eedb51bbdeb0be28cc6c62e03f8a
Joshua Villanueva <jvillanueva3651@gmail.com>
2025-05-07 15:58:56 -0500

    Implemented buildings CRUD form and logic

--- src/frmBuildingsCRUD.Designer.cs
+++ src/frmBuildingsCRUD.Designer.cs
@@ -34,8 +34,10 @@
             dgvBuildingID = new DataGridViewTextBoxColumn();
             dgvBuildingName = new DataGridViewTextBoxColumn();
             dgvDirector = new DataGridViewTextBoxColumn();
+            dgvViewRoomsBtn = new DataGridViewButtonColumn();
+            dgvAddRoomBtn = new DataGridViewButtonColumn();
+            dgvBtnView = new DataGridViewButtonColumn();
             dgvBtnEdit = new DataGridViewButtonColumn();
-            dgvBtnDelete = new DataGridViewButtonColumn();
             ((System.ComponentModel.ISupportInitialize)dgvBuildings).BeginInit();
             SuspendLayout();
             // 
@@ -47,69 +49,83 @@
             btnReturnHome.TabIndex = 5;
             btnReturnHome.Text = "Return to Home";
             btnReturnHome.UseVisualStyleBackColor = true;
+            btnReturnHome.Click += btnReturnHome_Click;
             // 
             // btnAddNewBuilding
             // 
-            btnAddNewBuilding.Location = new Point(629, 412);
+            btnAddNewBuilding.Location = new Point(747, 412);
             btnAddNewBuilding.Name = "btnAddNewBuilding";
             btnAddNewBuilding.Size = new Size(160, 27);
             btnAddNewBuilding.TabIndex = 4;
             btnAddNewBuilding.Text = "Add New Building";
             btnAddNewBuilding.UseVisualStyleBackColor = true;
+            btnAddNewBuilding.Click += btnAddNewBuilding_Click;
             // 
             // dgvBuildings
             // 
             dgvBuildings.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvBuildings.Columns.AddRange(new DataGridViewColumn[] { dgvBuildingID, dgvBuildingName, dgvDirector, dgvBtnEdit, dgvBtnDelete });
-            dgvBuildings.Location = new Point(12, 12);
+            dgvBuildings.Columns.AddRange(new DataGridViewColumn[] { dgvBuildingID, dgvBuildingName, dgvDirector, dgvViewRoomsBtn, dgvAddRoomBtn, dgvBtnView, dgvBtnEdit });
+            dgvBuildings.Location = new Point(12, 13);
             dgvBuildings.Name = "dgvBuildings";
-            dgvBuildings.Size = new Size(776, 393);
+            dgvBuildings.Size = new Size(895, 393);
             dgvBuildings.TabIndex = 3;
             // 
             // dgvBuildingID
             // 
-            dgvBuildingID.DataPropertyName = "dgvBuildingID";
+            dgvBuildingID.DataPropertyName = "BuildingID";
             dgvBuildingID.HeaderText = "BuildingID";
             dgvBuildingID.Name = "dgvBuildingID";
             dgvBuildingID.ReadOnly = true;
             // 
             // dgvBuildingName
             // 
-            dgvBuildingName.DataPropertyName = "dgvBuildingName";
+            dgvBuildingName.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dgvBuildingName.DataPropertyName = "Name";
             dgvBuildingName.HeaderText = "Building";
             dgvBuildingName.Name = "dgvBuildingName";
-            dgvBuildingName.ReadOnly = true;
-            dgvBuildingName.Width = 254;
             // 
             // dgvDirector
             // 
-            dgvDirector.DataPropertyName = "dgvDirector";
+            dgvDirector.DataPropertyName = "Director";
             dgvDirector.HeaderText = "Director";
             dgvDirector.Name = "dgvDirector";
-            dgvDirector.ReadOnly = true;
             dgvDirector.Width = 250;
+            // 
+            // dgvViewRoomsBtn
+            // 
+            dgvViewRoomsBtn.HeaderText = "";
+            dgvViewRoomsBtn.Name = "dgvViewRoomsBtn";
+            dgvViewRoomsBtn.Text = "View Rooms";
+            dgvViewRoomsBtn.UseColumnTextForButtonValue = true;
+            // 
+            // dgvAddRoomBtn
+            // 
+            dgvAddRoomBtn.HeaderText = "";
+            dgvAddRoomBtn.Name = "dgvAddRoomBtn";
+            dgvAddRoomBtn.Text = "Add Room";
+            dgvAddRoomBtn.UseColumnTextForButtonValue = true;
+            // 
+            // dgvBtnView
+            // 
+            dgvBtnView.HeaderText = "";
+            dgvBtnView.Name = "dgvBtnView";
+            dgvBtnView.Text = "View";
+            dgvBtnView.UseColumnTextForButtonValue = true;
+            dgvBtnView.Width = 65;
             // 
             // dgvBtnEdit
             // 
-            dgvBtnEdit.DataPropertyName = "dgvBtnEdit";
             dgvBtnEdit.HeaderText = "";
             dgvBtnEdit.Name = "dgvBtnEdit";
+            dgvBtnEdit.Text = "Edit";
             dgvBtnEdit.UseColumnTextForButtonValue = true;
             dgvBtnEdit.Width = 65;
-            // 
-            // dgvBtnDelete
-            // 
-            dgvBtnDelete.DataPropertyName = "dgvButtonDelete";
-            dgvBtnDelete.HeaderText = "";
-            dgvBtnDelete.Name = "dgvBtnDelete";
-            dgvBtnDelete.UseColumnTextForButtonValue = true;
-            dgvBtnDelete.Width = 65;
             // 
             // frmBuildingsCRUD
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(801, 451);
+            ClientSize = new Size(920, 451);
             Controls.Add(btnReturnHome);
             Controls.Add(btnAddNewBuilding);
             Controls.Add(dgvBuildings);
@@ -124,11 +140,13 @@
         private Button btnReturnHome;
         private Button btnAddNewBuilding;
         private DataGridView dgvBuildings;
+        private Label label4;
         private DataGridViewTextBoxColumn dgvBuildingID;
         private DataGridViewTextBoxColumn dgvBuildingName;
         private DataGridViewTextBoxColumn dgvDirector;
+        private DataGridViewButtonColumn dgvViewRoomsBtn;
+        private DataGridViewButtonColumn dgvAddRoomBtn;
+        private DataGridViewButtonColumn dgvBtnView;
         private DataGridViewButtonColumn dgvBtnEdit;
-        private DataGridViewButtonColumn dgvBtnDelete;
-        private Label label4;
     }
 }
--- src/frmBuildingsCRUD.cs
+++ src/frmBuildingsCRUD.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Microsoft.Data.SqlClient;
+using System.Configuration;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WUMedCoProject.src;
 
 namespace WUMedCoProject
 {
@@ -15,11 +18,89 @@ namespace WUMedCoProject
         public frmBuildingsCRUD()
         {
             InitializeComponent();
+            LoadBuildings();
         }
 
-        private void lblDirector_Click(object sender, EventArgs e)
+        /**********************************************************************
+         * Method to handle the click event of the "Add new Building" button. 
+         *********************************************************************/
+        private void btnAddNewBuilding_Click(object sender, EventArgs e)
         {
+            frmBuilding buildingForm = new frmBuilding(frmBuilding.FormMode.Add);
+            buildingForm.Show();
+        }
 
+        /**********************************************************************
+         * Method to handle the click event of the "Return to Home" button. 
+         *********************************************************************/
+        private void btnReturnHome_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /**********************************************************************
+         * Method to load buildings into the DataGridView
+         *********************************************************************/
+        private void LoadBuildings()
+        {
+            using (var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["WUMedCo"].ConnectionString))
+            {
+                var query = @"
+                    SELECT 
+                        b.BuildingID, 
+                        b.Name,
+                        CASE 
+                            WHEN b.DirectorID IS NULL THEN '[No Director Assigned]'
+                            ELSE e.LastName + ', ' + e.FirstName 
+                        END AS Director
+                    FROM Building b
+                    LEFT JOIN Employee e ON b.DirectorID = e.EmployeeID";
+
+                var adapter = new SqlDataAdapter(query, conn);
+                var dt = new DataTable();
+                adapter.Fill(dt);
+
+                dgvBuildings.DataSource = dt;
+                dgvBuildings.Columns["dgvBuildingID"].Visible = false; //Hidden ID
+            }
+        }
+
+        /**********************************************************************
+         * Method to handle the edit, view, delete buttons in the DataGridView
+         *********************************************************************/
+        private void dgvPatients_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0 || e.ColumnIndex < 0) return;
+
+            var dataTable = (DataTable)dgvBuildings.DataSource;
+
+            var buildingId = Convert.ToInt32(dataTable.Rows[e.RowIndex]["dgvBuildingID"]);
+
+            //For Debugging
+            //MessageBox.Show($"BuildingID: {buildingId}");
+
+            if (dgvBuildings.Columns[e.ColumnIndex] == dgvBtnView)
+            {
+                OpenBuildingForm(frmBuilding.FormMode.View, buildingId);
+            }
+            else if (dgvBuildings.Columns[e.ColumnIndex] == dgvBtnEdit)
+            {
+                OpenBuildingForm(frmBuilding.FormMode.Edit, buildingId);
+            }
+        }
+
+        /**********************************************************************
+         * Method to open the building form in the specified mode
+         *********************************************************************/
+        private void OpenBuildingForm(frmBuilding.FormMode mode, int? buildingId = null)
+        {
+            using (var buildingForm = new frmBuilding(mode, buildingId))
+            {
+                if (buildingForm.ShowDialog() == DialogResult.OK)
+                {
+                    LoadBuildings(); //Refreshes the DGV
+                }
+            }
         }
     }
 }
