4dcc85ee1c059f0d2c050178c2a17b36ff2f56ce
Joshua Villanueva <jvillanueva3651@gmail.com>
2025-05-08 03:32:39 -0500

    Minor changes, still lots TODO

--- src/frmMedicalRecordsCRUD.Designer.cs
+++ src/frmMedicalRecordsCRUD.Designer.cs
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmMedicalRecordsCRUD";
+            SuspendLayout();
+            // 
+            // frmMedicalRecordsCRUD
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 602);
+            Name = "frmMedicalRecordsCRUD";
+            Text = "frmMedicalRecordsCRUD";
+            ResumeLayout(false);
         }
 
         #endregion
