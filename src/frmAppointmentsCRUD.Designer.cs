b7d604e0e3cb0f4c61f7147919dbacd61f7bd9a2
Joshua Villanueva <jvillanueva3651@gmail.com>
2025-05-08 03:33:13 -0500

    Implemented Appointments CRUD, loading logic, form functionality

--- src/frmAppointmentsCRUD.Designer.cs
+++ src/frmAppointmentsCRUD.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace WUMedCoProject
+﻿
+namespace WUMedCoProject
 {
     partial class frmAppointmentsCRUD
     {
@@ -28,12 +29,156 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmAppointmentsCRUD";
+            dgvAppointments = new DataGridView();
+            btnReturnHome = new Button();
+            btnAddNewAppointment = new Button();
+            AppointmentID = new DataGridViewTextBoxColumn();
+            DateTime = new DataGridViewTextBoxColumn();
+            Purpose = new DataGridViewTextBoxColumn();
+            PatientName = new DataGridViewTextBoxColumn();
+            Physician = new DataGridViewTextBoxColumn();
+            RoomType = new DataGridViewTextBoxColumn();
+            BtnView = new DataGridViewButtonColumn();
+            BtnEdit = new DataGridViewButtonColumn();
+            BtnDelete = new DataGridViewButtonColumn();
+            ((System.ComponentModel.ISupportInitialize)dgvAppointments).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvAppointments
+            // 
+            dgvAppointments.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvAppointments.Columns.AddRange(new DataGridViewColumn[] { AppointmentID, DateTime, Purpose, PatientName, Physician, RoomType, BtnView, BtnEdit, BtnDelete });
+            dgvAppointments.Location = new Point(12, 12);
+            dgvAppointments.Name = "dgvAppointments";
+            dgvAppointments.Size = new Size(1058, 519);
+            dgvAppointments.TabIndex = 0;
+            dgvAppointments.CellClick += dgvAppointments_CellClick;
+            // 
+            // btnReturnHome
+            // 
+            btnReturnHome.Location = new Point(12, 537);
+            btnReturnHome.Name = "btnReturnHome";
+            btnReturnHome.Size = new Size(160, 27);
+            btnReturnHome.TabIndex = 6;
+            btnReturnHome.Text = "Return to Home";
+            btnReturnHome.UseVisualStyleBackColor = true;
+            btnReturnHome.Click += btnReturnHome_Click;
+            // 
+            // btnAddNewAppointment
+            // 
+            btnAddNewAppointment.Location = new Point(910, 537);
+            btnAddNewAppointment.Name = "btnAddNewAppointment";
+            btnAddNewAppointment.Size = new Size(160, 27);
+            btnAddNewAppointment.TabIndex = 7;
+            btnAddNewAppointment.Text = "Schedule an Appointment";
+            btnAddNewAppointment.UseVisualStyleBackColor = true;
+            btnAddNewAppointment.Click += btnAddNewAppointment_Click;
+            // 
+            // AppointmentID
+            // 
+            AppointmentID.DataPropertyName = "AppointmentID";
+            AppointmentID.HeaderText = "AppointmentID";
+            AppointmentID.Name = "AppointmentID";
+            AppointmentID.ReadOnly = true;
+            AppointmentID.Visible = false;
+            // 
+            // DateTime
+            // 
+            DateTime.DataPropertyName = "DateTime";
+            DateTime.HeaderText = "Scheduled For";
+            DateTime.Name = "DateTime";
+            DateTime.ReadOnly = true;
+            DateTime.Width = 125;
+            // 
+            // Purpose
+            // 
+            Purpose.DataPropertyName = "Purpose";
+            Purpose.HeaderText = "Purpose";
+            Purpose.Name = "Purpose";
+            Purpose.ReadOnly = true;
+            Purpose.Width = 200;
+            // 
+            // PatientName
+            // 
+            PatientName.DataPropertyName = "PatientName";
+            PatientName.HeaderText = "Patient";
+            PatientName.Name = "PatientName";
+            PatientName.ReadOnly = true;
+            PatientName.Width = 150;
+            // 
+            // Physician
+            // 
+            Physician.DataPropertyName = "Physician";
+            Physician.HeaderText = "Physician";
+            Physician.Name = "Physician";
+            Physician.ReadOnly = true;
+            Physician.Width = 150;
+            // 
+            // RoomType
+            // 
+            RoomType.DataPropertyName = "RoomType";
+            RoomType.HeaderText = "Room";
+            RoomType.Name = "RoomType";
+            RoomType.ReadOnly = true;
+            RoomType.Width = 150;
+            // 
+            // BtnView
+            // 
+            BtnView.DataPropertyName = "BtnView";
+            BtnView.HeaderText = "";
+            BtnView.Name = "BtnView";
+            BtnView.ReadOnly = true;
+            BtnView.Text = "View";
+            BtnView.UseColumnTextForButtonValue = true;
+            BtnView.Width = 80;
+            // 
+            // BtnEdit
+            // 
+            BtnEdit.DataPropertyName = "BtnEdit";
+            BtnEdit.HeaderText = "";
+            BtnEdit.Name = "BtnEdit";
+            BtnEdit.ReadOnly = true;
+            BtnEdit.Text = "Edit";
+            BtnEdit.UseColumnTextForButtonValue = true;
+            BtnEdit.Width = 80;
+            // 
+            // BtnDelete
+            // 
+            BtnDelete.DataPropertyName = "BtnDelete";
+            BtnDelete.HeaderText = "";
+            BtnDelete.Name = "BtnDelete";
+            BtnDelete.ReadOnly = true;
+            BtnDelete.Text = "Delete";
+            BtnDelete.UseColumnTextForButtonValue = true;
+            BtnDelete.Width = 80;
+            // 
+            // frmAppointmentsCRUD
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1084, 569);
+            Controls.Add(btnAddNewAppointment);
+            Controls.Add(btnReturnHome);
+            Controls.Add(dgvAppointments);
+            Name = "frmAppointmentsCRUD";
+            Text = "frmAppointmentsCRUD";
+            ((System.ComponentModel.ISupportInitialize)dgvAppointments).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private DataGridView dgvAppointments;
+        private Button btnReturnHome;
+        private Button btnAddNewAppointment;
+        private DataGridViewTextBoxColumn AppointmentID;
+        private DataGridViewTextBoxColumn DateTime;
+        private DataGridViewTextBoxColumn Purpose;
+        private DataGridViewTextBoxColumn PatientName;
+        private DataGridViewTextBoxColumn Physician;
+        private DataGridViewTextBoxColumn RoomType;
+        private DataGridViewButtonColumn BtnView;
+        private DataGridViewButtonColumn BtnEdit;
+        private DataGridViewButtonColumn BtnDelete;
     }
 }
--- src/frmAppointmentsCRUD.cs
+++ src/frmAppointmentsCRUD.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Configuration;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WUMedCoProject.src;
 
 namespace WUMedCoProject
 {
@@ -15,6 +18,100 @@ namespace WUMedCoProject
         public frmAppointmentsCRUD()
         {
             InitializeComponent();
+            LoadAppointments();
+        }
+
+        /**********************************************************************
+         * Method to load appointments into the DataGridView
+         *********************************************************************/
+        private void LoadAppointments()
+        {
+            using (var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["WUMedCo"].ConnectionString))
+            {
+                var query = @"
+                    SELECT 
+                        a.AppointmentID, 
+                        a.DateTime,
+                        a.Purpose,
+                        CONCAT(p.LastName, ', ', p.FirstName) AS PatientName,
+                        CONCAT(e.LastName, ', ', e.FirstName) AS Physician,
+                        r.RoomType
+                    FROM Appointment a
+                    LEFT JOIN Patient p ON a.PatientID = p.PatientID
+                    LEFT JOIN Employee e ON a.PhysicianID = e.EmployeeID
+                    LEFT JOIN Room r ON a.RoomID = r.RoomID";
+                var adapter = new SqlDataAdapter(query, conn);
+                var dt = new DataTable();
+                adapter.Fill(dt);
+                dgvAppointments.DataSource = dt;
+            }
+        }
+
+        /**********************************************************************
+         * Method to handle the click event of the "Schedule an Appointment" Button
+         *********************************************************************/
+        private void btnAddNewAppointment_Click(object sender, EventArgs e)
+        {
+            frmAppointment appointmentForm = new frmAppointment(frmAppointment.FormMode.Add);
+            appointmentForm.Show();
+        }
+
+        /**********************************************************************
+         * Method to handle the click event of the "Return to Home" Button
+         *********************************************************************/
+        private void btnReturnHome_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /**********************************************************************
+         * Method to handle the edit, view, delete buttons in the DataGridView
+         *********************************************************************/
+        private void dgvAppointments_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0 || e.ColumnIndex < 0) return;
+
+            var dataTable = (DataTable)dgvAppointments.DataSource;
+
+            var appointmentId = Convert.ToInt32(dataTable.Rows[e.RowIndex]["AppointmentID"]);
+
+            //For Debugging
+            //MessageBox.Show($"BuildingID: {buildingId}");
+
+            if (dgvAppointments.Columns[e.ColumnIndex] == BtnView)
+            {
+                OpenAppointmentForm(frmAppointment.FormMode.View, appointmentId);
+            }
+            else if (dgvAppointments.Columns[e.ColumnIndex] == BtnEdit)
+            {
+                OpenAppointmentForm(frmAppointment.FormMode.Edit, appointmentId);
+            }
+            else if (dgvAppointments.Columns[e.ColumnIndex] == BtnDelete)
+            {
+                DeleteAppointment(appointmentId);
+            }
+        }
+
+        /**********************************************************************
+         * Method to open the appointment form
+         *********************************************************************/
+        private void OpenAppointmentForm(frmAppointment.FormMode mode, int appointmentId)
+        {
+            using (var appointmentForm = new frmAppointment(mode, appointmentId))
+            {
+                if (appointmentForm.ShowDialog() == DialogResult.OK)
+                {
+                    LoadAppointments(); //Refreshes the DGV
+                }
+            }
+        }
+
+        /**********************************************************************
+         * Method to delete an appointment
+         *********************************************************************/
+        private void DeleteAppointment(int appointmentId)
+        {
+
         }
     }
 }
